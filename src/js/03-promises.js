eb17abc68091eaaf2a874aada2b5850e0f14f2bf
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-04-19 12:45:32 +0300

    js-3-09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('form');
+const inputAmount = form.querySelector('[name="amount"]');
+const inputDelay = form.querySelector('[name="delay"]');
+const inputStep = form.querySelector('[name="step"]');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const amount = parseInt(inputAmount.value);
+  const delay = parseInt(inputDelay.value);
+  const step = parseInt(inputStep.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});

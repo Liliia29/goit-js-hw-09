49b5733d6cfd21ad64bf4e54c199d2e1ab8dcf16
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-04-19 12:27:13 +0300

    js-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  let intervalId = null;
 
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  function startColorChange() {
+    startBtn.disabled = true; 
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+
+  function stopColorChange() {
+    clearInterval(intervalId);
+    startBtn.disabled = false; 
+  }
+
+  startBtn.addEventListener('click', startColorChange);
+  stopBtn.addEventListener('click', stopColorChange);

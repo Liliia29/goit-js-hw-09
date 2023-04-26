e391af4653ea5d60648360dab05cfe33c1b40d2d
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-04-26 15:24:51 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,6 @@
 import flatpickr from 'flatpickr';
 import "flatpickr/dist/flatpickr.min.css";
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-
   const startButton = document.querySelector('[data-start]');
   const daysDisplay = document.querySelector('[data-days]');
   const hoursDisplay = document.querySelector('[data-hours]');
@@ -28,51 +18,42 @@ const options = {
   }, 1000);
   
   let countdownInterval;
-
-  flatpickr(dateInput, {
-    enableTime: true,
-    minDate: "today",
-    dateFormat: "Y-m-d H:i",
-    onClose: function(selectedDates) {
-        const selectedDate = selectedDates[0];
-        const now = new Date();
-        if (selectedDate < now) {
-          window.alert("Please choose a date in the future");
-          startButton.disabled = true;
-        } else {
-          startButton.disabled = false;
-        }
-    }
-  });
   
-    startButton.addEventListener("click", function() {
-    const selectedDate = flatpickr.parseDate(dateInput.value);
-    const now = new Date();
-    let countdown = selectedDate.getTime() - now.getTime();
-    if (countdown < 0) {
-      return;
-    }
-    clearInterval(countdownInterval);
-    countdownInterval = setInterval(function convertMs(ms) {
+  countdownInterval = function convertMs(ms) {
     
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-      
-        const days = Math.floor(ms / day);
-        const hours = Math.floor((ms % day) / hour);
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      
-        return { days, hours, minutes, seconds };
-      },
-      
-      function addLeadingZero(value) {
-        return value.toString().padStart(2, '0');
-      }, 1000);
-  });
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
   
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  },
+  
+  function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
+  };
 
-
+  flatpickr(datetime-picker, {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      const selectedDate = selectedDates[0];
+  
+      if (selectedDate.getTime() < Date.now()) {
+        window.alert("Please choose a date in the future");
+      } else {
+        startButton.disabled = false;
+      }
+    },
+  });
+           
+    
+  
   
